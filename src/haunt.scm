53e876bc6b83ce3ffb7cf54ad7be8c48e1713df1
cel7t <shahsarthakw@gmail.com>
2024-10-20 22:38:26 +0530

    Initial Commit

---
+++ src/haunt.scm
@@ -0,0 +1,48 @@
+(use-modules (haunt asset)
+             (haunt builder blog)
+             (haunt builder atom)
+             (haunt builder assets)
+             (haunt post)
+             (haunt site)
+             (html-prime)
+             (theme)
+             (utils))
+
+(define %collections
+  `(("Recent Posts" "index.html" ,posts/reverse-chronological)))
+
+(define about-page
+  (static-page
+   "About Me"
+   "about.html"
+   `(div (@ (style "display:flex")) 
+         (img (@ (src "images/suigintou-sicp.jpg") 
+                 (style "object-fit:contain; align-self:flex-start; 
+                         width: 250px; padding-top: 20px; padding-bottom: 20px")))
+         (div (@ (style "flex: 1 1 auto; padding-left: 30px"))
+              (h2 "Hello 👋")
+              (p "My name is Sarthak (IPA: /s̪äːɾ.t̪ʰək/).")
+              (p "I like computers, mathematics and music.")
+              (p "I worked on "
+                 ,(anchor "Parameterized Packages" "https://summerofcode.withgoogle.com/programs/2023/projects/heQYLzrz")
+                 " as a " 
+                 ,(anchor "GSoC" "https://summerofcode.withgoogle.com") 
+                 " intern at "
+                 ,(anchor "GNU Guix" "https://guix.gnu.org")
+                 ".")
+              (p "You can contact me via e-mail at shahsarthakw at gmail dot com.")))))
+
+(site #:title "(blog . (lispy . tech))"
+      #:domain "blog.coldboot.org"
+      #:default-metadata
+      '((author . "Sarthak Shah")
+        (email  . "shahsarthakw@gmail.com"))
+      #:readers (list html-reader-prime)
+      #:builders (list (blog #:theme lispy-theme #:collections %collections)
+                       (atom-feed)
+                       (atom-feeds-by-tag)
+                       about-page
+                       (static-directory "css")
+                       (static-directory "js")
+                       (static-directory "fonts")
+                       (static-directory "images")))
